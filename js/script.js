4f69896278f2f9d237ce0e4ce0876634b290adba
heggland <kjetilheggland@gmail.com>
2020-08-15 12:58:05 +0200

    Upload

---
+++ js/script.js
@@ -0,0 +1,93 @@
+// Javascript module assignment found at https://github.com/NoroffFEU/ma-javascript1-module1
+// by Kjetil Heggland
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+//Question 1
+
+const cat = {
+    complain: function() {
+        console.log('Meow!');
+    }
+};
+
+cat.complain();
+
+//Question 2
+
+const heading = document.querySelector('h3');
+heading.innerHTML = 'Updated heading';
+
+//Question 3
+
+heading.style.fontSize='2em';
+
+// OR document.querySelector('h3').style.fontSize='2em';
+
+//Question 4
+heading.classList.add('subheading');
+
+//OR document.querySelector('h3').classList.add('subheading');
+
+//Question 5
+const paragraphs = document.querySelectorAll('p');
+
+for(let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color= 'red';
+}
+
+//Question 6
+
+const resultsContainer = document.querySelector('.results');
+
+resultsContainer.style.backgroundColor='yellow';
+resultsContainer.innerHTML='<p>New paragraph</p>';
+
+//Question 7
+function catlist(cats) {
+     for(let i = 0; i < cats.length; i++) {
+        console.log(cats[i].name);
+     }
+}
+
+catlist(cats);
+
+
+//Question 8
+function createCats(cats) {
+
+    let html = "";
+
+    for(let i = 0; i < cats.length; i++) {
+
+        let age = "Age unknown";
+
+        if(cats[i].age) {
+            age = cats[i].age;
+        }
+
+        html += `<div>
+                    <h5>${cats[i].name}</h5>
+                    <p>Released: ${age}</p>
+                </div>`;
+    }
+
+    return html;
+}
+
+const newHTML = createCats(cats);
+
+const catContainer = document.querySelector(".cat-container");
+catContainer.innerHTML = newHTML;
